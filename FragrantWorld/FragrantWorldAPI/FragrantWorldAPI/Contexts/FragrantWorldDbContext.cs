7a658e62e4ea8dca035bade2cf57a61939ed020c
DenobBlack <egor19-1205.baranov@yandex.ru>
2024-12-20 21:03:24 +0300

    Забыл для Readme

--- FragrantWorld/FragrantWorldAPI/FragrantWorldAPI/Contexts/FragrantWorldDbContext.cs
+++ FragrantWorld/FragrantWorldAPI/FragrantWorldAPI/Contexts/FragrantWorldDbContext.cs
@@ -29,7 +29,7 @@ public partial class FragrantWorldDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Data Source=ISPP2114\\SQLEXPRESS;Initial Catalog=FragmentWorld;User Id=log;Password=123;Trusted_Connection=true;TrustServerCertificate=true");
+        => optionsBuilder.UseSqlServer("YOUR_CONNECTION_STRING");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
